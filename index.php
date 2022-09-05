91d16c0fc0419c31c2240d0ce3d7f3dc6de9eec8
Bino <yeebino001@gmail.com>
2022-09-05 21:48:12 +0200

    movie added

--- index.php
+++ index.php
@@ -115,3 +115,62 @@ $CardTwo = new Card("drago nero occhi rossi", "monster", 6, 2500, 2000);
 // $CardTwo->def = 2000;
 
 var_dump($CardTwo);
+
+// // // // // // // // // // // // // // // // // // Part two
+
+// crea una nuova classe 
+class Movie{
+    // insert movie's characteristics
+    private $name;
+    private $actors;
+    private $genres;
+    private $runtime = 0;
+    private $price = 0;
+    // // // METHODS
+    // // this is magic in php!
+    function __construct($name, $actors, $genres, $runtime, $price)
+    {
+        $this->setName($name);
+        $this->setType($actors);
+        $this->setGenres($genres);
+        $this->setRuntime($runtime);
+        $this->setPrice($price);
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($namesetted){
+        $this->name = $namesetted; 
+    }
+    public function getActors(){
+        return $this->actors;
+    }
+    public function setType($actorssetted){
+        $this->actors = $actorssetted;
+    }
+    public function getGenres(){
+        return $this->genres;
+        
+    }
+    public function setGenres($genressetted){
+        $this->genres = $genressetted;
+    }
+    public function getRuntime(){
+        return $this->runtime;
+    }
+    public function setRuntime($runtimesetted){
+        $this->runtime = $runtimesetted;
+    }    
+    public function getPrice(){
+        return $this->price;
+    }
+    public function setPrice($pricesetted){
+        $this->price = $pricesetted;
+    }
+}
+$MovieOne = new Movie("spiderman homecoming", "tom holland", "action", "1h 37m", 9.99);
+var_dump($MovieOne);
+$MovieTwo = new Movie("joker", "jack sparrow", "horror", "2h 16m", 16.99);
+var_dump($MovieOne);
+$MovieOne = new Movie("batman", "bruce wayne", "adventure", "1h 58m", 15);
+var_dump($MovieOne);
