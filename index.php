ab59ab52b3d6725f88b1fef3eb2d6f44161cc9a2
Bino <yeebino001@gmail.com>
2022-09-05 16:10:36 +0200

    CardOne populated

--- index.php
+++ index.php
@@ -6,12 +6,21 @@ class Card{
     // inserisci le caratteristiche della classe
     public $name;
     public $type;
-    public $atk;
-    public $def;
+    public $atk = 0;
+    public $def = 0;
 }
 
 $CardOne = new Card();
 
+// popoliamo $CardOne
+$CardOne->name = "drago bianco occhi blu";
+$CardOne->type = "monster";
+$CardOne->atk = 3000;
+$CardOne->def = 2500;
+
+
+
 $CardTwo = new Card();
 
 var_dump($CardOne);
+var_dump($CardTwo);
