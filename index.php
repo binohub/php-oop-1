9ce7da0a52c406f05116a2d71a37031656c8b6bd
Bino <yeebino001@gmail.com>
2022-09-05 17:19:48 +0200

    private characteristics

--- index.php
+++ index.php
@@ -3,24 +3,76 @@
 // crea una nuova classe 
 class Card{
     
-    // inserisci le caratteristiche della classe
-    public $name;
-    public $type;
-    public $atk = 0;
-    public $def = 0;
+    // insert card's characteristics
+    private $name;
+    private $type;
+    private $atk = 0;
+    private $def = 0;
+
+    // // methods
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        if(strlen($name) <= 3){
+            $this->name = "error 404! the name is too short";
+        }else if(strlen($name) >= 25){
+            $this->name = "error 404! the name is too long";
+        }else{
+            $this->name = $name; 
+        }
+    }
+
+
+    public function getType(){
+        return $this->type;
+    }
+    public function setType($type){
+        $this->type = $type;
+    }
+
+
+    public function getAtk(){
+        return $this->atk;
+    }
+    public function setAtk($atk){
+        $this->atk = $atk;
+    }    
+    
+
+    public function getDef(){
+        return $this->def;
+    }
+    public function setDef($def){
+        $this->def = $def;
+    }
 }
 
 $CardOne = new Card();
-
-// popoliamo $CardOne
-$CardOne->name = "drago bianco occhi blu";
-$CardOne->type = "monster";
-$CardOne->atk = 3000;
-$CardOne->def = 2500;
+// // populate $CardOne (if private doesn't work; u have to use methods)
+$CardOne->setName("drago bianco occhi blu");
+$CardOne->setType("monster");
+$CardOne->setAtk(3000);
+$CardOne->setDef(2500);
+// $CardOne->name = "drago bianco occhi blu";
+// $CardOne->type = "monster";
+// $CardOne->atk = 3000;
+// $CardOne->def = 2500;
 
 
 
 $CardTwo = new Card();
+
+// // populate $CardTwo if private doesn't work)
+$CardTwo->setName("drago nero occhi rossi");
+$CardTwo->setType("monster");
+$CardTwo->setAtk(2800);
+$CardTwo->setDef(2000);
+// $CardTwo->name = "drago nero occhi rossi";
+// $CardTwo->type = "monster";
+// $CardTwo->atk = 2500;
+// $CardTwo->def = 2000;
+
 
 var_dump($CardOne);
 var_dump($CardTwo);
