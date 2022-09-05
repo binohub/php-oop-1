d6c7cffcede807a1fcbf17def853ae85f8bac1bb
Bino <yeebino001@gmail.com>
2022-09-05 16:06:07 +0200

    start

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// crea una nuova classe 
+class Card{
+    
+    // inserisci le caratteristiche della classe
+    public $name;
+    public $type;
+    public $atk;
+    public $def;
+}
+
+$CardOne = new Card();
+
+$CardTwo = new Card();
+
+var_dump($CardOne);
